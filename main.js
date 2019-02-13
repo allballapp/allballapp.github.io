953f4c68c1015eecce51c31fb8e1ecfb318b354a
allballapp <kyle@allball.io>
2019-02-13 14:47:52 -0800

    Change button text on submit/success

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var script_url = "https://script.google.com/macros/s/AKfycbxLLA4KRUZa4TlYsaGCuxB4WtXnPy5sRWZMMqfI9Z--X_F6C1Dc/exec";
 function insert_value() {
     var email = $("#email").val();
+    $('#submitButton').html('<i class="far fa-basketball-ball fa-spin"></i>');
     var url = script_url + "?callback=ctrlq&email=" + email + "&action=insert";
     var request = jQuery.ajax({
         crossDomain: true,
@@ -10,7 +11,8 @@ function insert_value() {
     });
 }
 function ctrlq(e) {
-  alert("You're on the list! Your private beta code will be delivered to the email provided when it's released!");
+  $('#submitButton').html('<i class="far fa-check"></i>');
+  // alert("You're on the list! Your private beta code will be delivered to the email provided when it's released!");
 }
     
 (function () {
